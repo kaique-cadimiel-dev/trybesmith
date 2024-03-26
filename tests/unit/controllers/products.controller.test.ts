2924c40dcd246d5b02c309cbeb088ae0201c658e
kaique-cadimiel-dev <kaiqueamasio@gmail.com>
2024-03-26 15:44:45 -0300

    test: testa se é possível listar produtos

--- tests/unit/controllers/products.controller.test.ts
+++ tests/unit/controllers/products.controller.test.ts
@@ -4,6 +4,7 @@ import sinonChai from 'sinon-chai';
 import { Request, Response } from 'express';
 import productControler from '../../../src/controllers';
 import ProductModel from '../../../src/database/models/product.model';
+import productService from '../../../src/services';
 
 chai.use(sinonChai);
 
@@ -16,7 +17,7 @@ describe('ProductsController', function () {
     res.json = sinon.stub().returns(res);
     sinon.restore();
   });
-  it('verifica se é possível fazer uma request para controller', async () => {
+  it('Testa se na productController é possível criar produto', async () => {
     // arrange
     const product = {
       name: 'Espada',
@@ -31,5 +32,32 @@ describe('ProductsController', function () {
     // assert
     expect(res.status).to.have.calledWith(201);
     expect(res.json).to.have.calledWith(productMock);
+  })
+  it('Testa se na productController lista produtos', async () => {
+    // arrange
+    const products = [
+      {
+        id: 1,
+        name: "Excalibur",
+        price: "10 peças de ouro",
+        userId: 1
+      },
+      {
+        id: 2,
+        name: "Espada Justiceira",
+        price: "20 peças de ouro",
+        userId: 1
+      },
+    ]
+    const req = {} as Request;
+    const res = {} as Response;
+    res.status = sinon.stub().returnsThis();
+    res.json = sinon.stub().returns(products);
+    sinon.stub(productService, 'getAll').resolves({status: 200, data: products})
+    // act
+    await productControler.getAll(req, res);
+    // assert
+    expect(res.status).to.have.calledWith(200);
+    expect(res.json).to.have.calledWith(products);
   })
 });
--- tests/unit/services/products.service.test.ts
+++ tests/unit/services/products.service.test.ts
@@ -1,12 +1,11 @@
 import { expect } from 'chai';
 import sinon from 'sinon';
-// import insert from '../../../src/services/product.service'
 import ProductModel from '../../../src/database/models/product.model';
 import productService from '../../../src/services';
 
 describe('ProductsService', function () {
   beforeEach(function () { sinon.restore(); });
-  it('verifica se a camada service retorna status 201, com data valído', async () => {
+  it('Testa se a productService retorna status 201 e produto criado', async () => {
     // arrange
     const product = {
       name: 'Espada',
@@ -19,6 +18,30 @@ describe('ProductsService', function () {
     const {status, data} = await productService.insert(product);
     // assert
     expect(status).to.be.equal(201);
+    expect(data).to.be.equal(productMock);
+  })
+  it('Testa se a ProductService retorna status 201 e produtos', async () => {
+    // arrange
+    const products = [
+      {
+        id: 1,
+        name: "Excalibur",
+        price: "10 peças de ouro",
+        userId: 1
+      },
+      {
+        id: 2,
+        name: "Espada Justiceira",
+        price: "20 peças de ouro",
+        userId: 1
+      },
+    ]
+    const productMock = ProductModel.bulkBuild(products); 
+    sinon.stub(ProductModel, 'findAll').resolves(productMock);
+    // act
+    const {status, data} = await productService.getAll();
+    // assert
+    expect(status).to.be.equal(200);
     expect(data).to.be.equal(productMock);
   })
 });
